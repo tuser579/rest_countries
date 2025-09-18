ba52be5704a30b6a43edd8852c0cc3faa647c353
tuser579 <tusermon720@gmail.com>
2025-09-18 22:25:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import Countries from './components/Countries/countries.jsx'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Countries></Countries>
+
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>in countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Countries() {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+
+
+// ----------------
+// const Countries = () => {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
